03bf213dc79f6e1c41c93ec7829c8d78f524dc8f
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-19 11:57:23 -0700

    explicitly list routes to see if this fixes bug with edit/update, add test to look for bug

--- spec/promise_spec.rb
+++ spec/promise_spec.rb
@@ -1,1 +1,14 @@
+require "rails_helper"
+#test for update promise to find bug
 
+describe "edit a promise button" do
+  it "allows user to edit a promise" do
+
+    promise = Promise.create(description: "Bake me some cookies", giver: "Mr. Kipling")
+
+    visit edit_promise_path(promise)
+
+    expect(promise.status).to eq(0)
+    promise.update(status: 2)
+  end
+end
