5bdb67f31e1e15e8f056733d05281c62a4ed9af9
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-19 11:26:26 -0700

    Create Promise is functional

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -7,6 +7,17 @@ class PromisesController < ApplicationController
     @promise = Promise.new
   end
 
+  def create
+    @promise = Promise.create(promise_params)
+    if @promise.save
+      flash[:notice] = "Added your promise from #{@promise.giver}, they will probably definitely follow through on that."
+      redirect_to promises_path
+    else
+      flash[:notice] = "Oops, please try again"
+      render :new
+    end
+  end
+
   def edit
     @promise = Promise.find(promise_params[:id])
   end
